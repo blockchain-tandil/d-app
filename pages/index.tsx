5fbf94451d39bb69c73696f4d87a2848ebe8bdba
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2019-04-22 00:13:38 -0300

    chore: Initial config

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import List from '../components/List';
+
+const IndexPage: React.FunctionComponent = () => {
+  return (
+    <Layout title="Home | demo app for Blockchain Tandil">
+      <h1>Hello Blockchain Tandil 👋</h1>
+      <List />
+      <p><Link href='/about'><a>About</a></Link></p>
+    </Layout>
+  )
+}
+
+export default IndexPage;
